e657431d88beff89f88502eacdaccf62c3de1df4
snw35 <snw35@use.startmail.com>
2026-01-30 15:00:49 +0000

    Remove 3, fix paths

--- cu128/build.sh
+++ cu128/build.sh
@@ -7,10 +7,8 @@ if [ -f /home/ubuntu/venv/bin/activate ]; then
   source /home/ubuntu/venv/bin/activate
   pip wheel -w /home/ubuntu/wheelhouse --no-deps --no-build-isolation /home/ubuntu/sageattention-1
   pip wheel -w /home/ubuntu/wheelhouse --no-deps --no-build-isolation /home/ubuntu/${SAGE_FILENAME}
-  pip wheel -w /home/ubuntu/wheelhouse --no-deps --no-build-isolation /home/ubuntu/${SAGE_FILENAME}/sageattention3_blackwell
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/ubuntu/venv/lib/python3.11/site-packages/torch/lib"
-  auditwheel show /home/ubuntu/wheelhouse/*
-  auditwheel repair -w /home/ubuntu/wheelhouse /home/ubuntu/wheelhouse/* --strip
-  auditwheel show /home/ubuntu/wheelhouse/*
+  find /home/ubuntu/wheelhouse -name '*.whl' -exec auditwheel repair -w /home/ubuntu/wheelhouse {} --strip \;
+  find /home/ubuntu/wheelhouse -name '*.whl' -exec auditwheel show {} \;
 
 fi
